12c9cf0707ff9b990213ca66b05962f03c54ba3a
Gabriel Smith <BonzoFestoon@users.noreply.github.com>
2017-02-01 11:03:06 -0500

    Corrected comment

--- main.cpp
+++ main.cpp
@@ -7,10 +7,9 @@ int _tmain(int argc, _TCHAR* argv[])
    swprintf_s(ltsupport::szLicPath, (L_MAXPATH) / (sizeof ltsupport::szLicPath[0]), _T("D:\\GitHub\\LEADTOOLS\\eval-license-files.lic"));
    swprintf_s(ltsupport::szDevKey, (L_MAXPATH) / (sizeof ltsupport::szDevKey[0]), _T("{YourDeveloperKey}"));
   
-   // TODO: Also, update the license information in ltsupport.cpp
+   // TODO: Also, update the paths to these files
    L_TCHAR const szSourceDocument[] = _T("D:\\GitHub\\LEADTOOLS\\press-release.doc");
    L_TCHAR const szTargetDocument[] = _T("D:\\GitHub\\LEADTOOLS\\press-release.pdf");
-   
    L_TCHAR const szSourceVector[] = _T("D:\\GitHub\\LEADTOOLS\\random.dxf");
    L_TCHAR const szTargetVector[] = _T("D:\\GitHub\\LEADTOOLS\\random.pdf");
 
